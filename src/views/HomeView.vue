b75e0aaaa781fc98f7094910da1b44261567c554
jsh080000 <jsh080000@rootlab.xyz>
2025-03-20 17:19:38 +0900

    base 설정

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,13 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
+<!--
+  - crud 기능구현
+  - 로컬스토리지를 활용한 데이터 저장
+  - 필터링 기능(완료 / 미완료)
+  - 드래그 앤 드롭으로 순서 변경
+-->
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>home</div>
 </template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
